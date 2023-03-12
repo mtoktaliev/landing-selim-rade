2da2e14f6b4bc2550a5a8ea6fd3d10da2b44a946
mtoktaliev <mtoktalievmegacom@gmail.com>
2023-03-12 10:28:34 +0600

    horizontal scroll

--- src/components/weoffer/Weoffer.js
+++ src/components/weoffer/Weoffer.js
@@ -1,8 +1,18 @@
+import Slider from "react-slick";
 import LightBtn from '../lightBtn/LightBtn';
 import './Weoffer.css'
 
 
 const Weoffer = () => {
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+
     return (
         <div className="weoffer">
             <div className="container">
@@ -12,20 +22,39 @@ const Weoffer = () => {
                             <div className="weoffer_item-name">Секционные</div>
                         </div>
                         <div className="weoffer_item-two">
-                        <div className="weoffer_item-name">Автоматика</div>
+                            <div className="weoffer_item-name">Автоматика</div>
                         </div>
                         <div className="weoffer_item-three">
-                        <div className="weoffer_item-name">Роль ворота</div>
+                            <div className="weoffer_item-name">Роль ворота</div>
                         </div>
                         <div className="weoffer_item-four">
-                        <div className="weoffer_item-name">Распашные</div>
+                            <div className="weoffer_item-name">Распашные</div>
                         </div>
                         <div className="weoffer_item-five">
-                        <div className="weoffer_item-name">Складные</div>
+                            <div className="weoffer_item-name">Складные</div>
                         </div>
                     </div>
-                    <LightBtn name="смотреть все"/>
             </div>
+
+            <Slider {...settings} className="slider">
+                <div className="weoffer_item-one">
+                    <div className="weoffer_item-name">Секционные</div>
+                </div>
+                <div className="weoffer_item-two">
+                    <div className="weoffer_item-name">Автоматика</div>
+                </div>
+                <div className="weoffer_item-three">
+                    <div className="weoffer_item-name">Роль ворота</div>
+                </div>
+                <div className="weoffer_item-four">
+                    <div className="weoffer_item-name">Распашные</div>
+                </div>
+                <div className="weoffer_item-five">
+                    <div className="weoffer_item-name">Складные</div>
+                </div>
+        </Slider>
+
+        <LightBtn name="смотреть все" />
         </div>
     );
 }
